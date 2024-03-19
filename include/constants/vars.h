ea21c9f5eaaa52ac736f7047075a6ed392ddf8e5
beartime234 <josheatonis234@gmail.com>
2024-03-19 13:39:25 +1300

    map imporvments give super rod

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -265,7 +265,7 @@
 #define VAR_SCOTT_BF_CALL_STEP_COUNTER                   0x40F5
 #define VAR_RIVAL_RAYQUAZA_CALL_STEP_COUNTER             0x40F6
 #define VAR_GIVE_NUZLOCKE_ITEMS                          0x40F7 // Should the player receive Nuzlocke items? (0 = No, 1 = Yes)
-#define VAR_UNUSED_0x40F8                                0x40F8 // Unused Var
+#define VAR_GIVE_LITTLEROOT_SUPERROD                     0x40F8 // Should we give the player the Super Rod in Littleroot Town? (0 = No, 1 = Yes)
 #define VAR_UNUSED_0x40F9                                0x40F9 // Unused Var
 #define VAR_UNUSED_0x40FA                                0x40FA // Unused Var
 #define VAR_UNUSED_0x40FB                                0x40FB // Unused Var
